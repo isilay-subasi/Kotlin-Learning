aa0e1a9d7bfaf961ea06a16ac380bddc7e67eec0
isilay-subasi <isil.subasi19@gmail.com>
2021-11-07 19:47:12 +0300

    ExtensionFunction-Example

--- app/src/main/java/com/example/kotlinlearning/oop/ExtensionFunctions.kt
+++ app/src/main/java/com/example/kotlinlearning/oop/ExtensionFunctions.kt
@@ -15,6 +15,16 @@ fun main(args: Array<String>){
     list.swap(0,3)
     for(item in list) println(item)
 
+    fun Circle.perimeter() : Double{
+        return 2*Math.PI*radius
+    }
+
+    val newCircle=Circle(2.5)
+    println("Çemberin Alan : ${newCircle.area()}")
+    println("Çemberin Çevrei : ${newCircle.perimeter()}")
+
+
+
 
 }
 
@@ -25,5 +35,14 @@ fun ArrayList<String>.swap(x : Int , y : Int){
     val temp =this[x]
     this[x] = this[y]
     this[y] = temp
+
+}
+
+
+class Circle(val radius : Double){
+
+    fun area() : Double{
+        return Math.PI * radius * radius
+    }
 
 }
