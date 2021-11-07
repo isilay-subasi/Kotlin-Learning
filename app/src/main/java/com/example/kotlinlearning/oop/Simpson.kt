c5710241a3b5115496c024f787bb7a93fc52be06
isilay-subasi <isil.subasi19@gmail.com>
2021-11-07 19:14:53 +0300

    SimpsonClass-Constructor

---
+++ app/src/main/java/com/example/kotlinlearning/oop/Simpson.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinlearning.oop
+
+class Simpson(var inputSayi : Int , var inputSayi2 : Int) {
+
+
+    /*
+
+    var sayi = 0
+    private var sayi2 = 3
+    constructor(input1 : Int , input2 : Int ){
+        this.sayi=input1
+        this.sayi2=input2
+    }
+
+        fun getSayi2(){
+
+    }
+    */
+
+
+
+
+
+}
+
+fun main(args : Array<String>){
+
+    var simpson = Simpson(5,7)
+    /*
+
+    simpson.sayi
+    simpson.getSayi2()
+     */
+
+
+
+
+}
