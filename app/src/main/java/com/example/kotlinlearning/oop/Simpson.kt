4f346571320ad8b7e9cca5528ed722ff7e4e094e
isilay-subasi <isil.subasi19@gmail.com>
2021-11-07 19:34:29 +0300

    NullSafety

--- app/src/main/java/com/example/kotlinlearning/oop/Simpson.kt
+++ app/src/main/java/com/example/kotlinlearning/oop/Simpson.kt
@@ -3,6 +3,8 @@ package com.example.kotlinlearning.oop
 class Simpson(var inputSayi : Int , var inputSayi2 : Int) {
 
 
+
+
     /*
 
     var sayi = 0
@@ -32,7 +34,37 @@ fun main(args : Array<String>){
     simpson.getSayi2()
      */
 
+    //Nullability- Null olabilme
+    /*
+    Başlangıçta olduşturduğum değişkene değer ataması yapmazsam eğer hata verir.
+    Bunun için null değerini atayıp başlangıçta hiç bir değer ataması yapmayabiliriz.
 
+    //Nullable(?) && Non-null
+    // !! myAge'in değeri kesinlikle orda demektir.
+    // if ile bunu kontrol etmeliyiz. buna NullSafety denir.
+    // NullSafety - Null Güvenirliliği
+    //Soru işareti bu değer var mı yok mu bilmiyorum demektir.
+
+
+     */
+
+    var myString : String? = null // Bu artık nullable Stringtir.
+    var myAge : Int? = null
+
+    //1.yöntem -Null Safety
+    if(myAge != null){
+        println(myAge * 10)
+    }else{
+        println("myAge null")
+    }
+
+    //2.yöntem -Safe Call
+    println(myAge?.compareTo(2))
+
+    //3.yöntem - Elvis
+    //Eğer null dönerse -100 ekrana yazdırılsın anlamında güvenliği sağlayacaktır.
+    val myResult = myAge?.compareTo(2) ?: -100
+    println(myResult)
 
 
 }
